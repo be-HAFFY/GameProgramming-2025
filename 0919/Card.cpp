d5e979264fb50e9e5df7fb43357da5122e405360
be-HAFFY <haffiness427@gmail.com>
2025-09-23 11:53:40 +0900

    Add files via upload

--- 0919/Card.cpp
+++ 0919/Card.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct trump{ //카드 구조체 
    char order;
@@ -10,12 +12,14 @@ struct trump{ //카드 구조체
 char shape[4][4] = {"♠", "♦", "♥", "♣"}; //2차원 배열, 4개의 문자가 3Byte(확장코드)+1Byte(Null) 
 void make_card(trump m_card[]);
 void display_card(trump m_card[]);
+void shuffle_card(trump m_card[]);
 
 
 int main(void){
    trump card[52];
    make_card(card);
-   display_card(card); 
+   shuffle_card(card);
+   display_card(card);
    return 0;
 }
 
@@ -53,5 +57,17 @@ void display_card(trump m_card[]){ //카드 출력
       if (i%13==12){
          printf("\n");
       }
+   }
+}
+
+void shuffle_card(trump m_card[]){
+   int i, rnd;
+   trump tmp;
+   srand(time(NULL)); // 난수의 초기화
+   for(int i = 0; i<52; i++){
+      rnd = rand()%52;
+      tmp = m_card[rnd];
+      m_card[rnd] = m_card[i];
+      m_card[i] = tmp;
    }
 }
